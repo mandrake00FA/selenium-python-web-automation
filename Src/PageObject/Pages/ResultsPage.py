95c7a981f684cdd0bad17012216b6b0a2dc30097
mandrake00FA <43618370+mandrake00FA@users.noreply.github.com>
2018-10-03 10:19:06 -0500

    Create ResultsPage.py
    Results page objects

---
+++ Src/PageObject/Pages/ResultsPage.py
@@ -0,0 +1,28 @@
+__author__ = 'Enrique Cosio'
+
+from selenium.webdriver.common.by import By
+from selenium-python-web-automation.Src.PageObject.Locators import Locator
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import Select
+from selenium-python-web-automation.Src.PageObject.Locators import Locator
+
+
+class SearchResults(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.sort_menu = driver.find_element(By.XPATH, Locator.sort_menu)
+        self.review_hover_menu = driver.find_element(By.XPATH, Locator.review_hover_menu)
+        self.create_list_link = driver.find_element(By.XPATH, Locator.create_list_link)
+
+    # Sorting drop down menu
+    def setSortingOrder(self, sort_order):
+        select = Select(self.sort_menu)
+        select.select_by_visible_text(sort_order)
+
+    # ActionChains can be used in a chain pattern, in this case to hover over accounts menu and the clicking on a link
+    def selectCreateList(self, driver):
+        Hover = ActionChains(driver).move_to_element(self.review_hover_menu).move_to_element(self.create_list_link)
+        Hover.click().perform()
